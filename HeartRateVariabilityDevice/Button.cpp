97020b55df37b729c3f650b034272b512726aa46
ali-jameel1 <alijameel4545@gmail.com>
2023-04-10 04:08:10 -0400

    added switch statements to button class

--- HeartRateVariabilityDevice/Button.cpp
+++ HeartRateVariabilityDevice/Button.cpp
@@ -8,7 +8,6 @@ Button::Button(QWidget* parent) : QPushButton(parent), m_count(0) {
 Button::Button(const QString& text, QWidget* parent) : QPushButton(text, parent), m_count(0) {
   connect(this, &Button::clicked, this, &Button::onButtonClicked);
   name = text;
-  qDebug() <<name;
 }
 
 Button::~Button() {}
@@ -16,6 +15,29 @@ Button::~Button() {}
 void Button::onButtonClicked() {
   m_count++;
   emit clickedWithCount(m_count, name);
+
+  switch(name.toStdString()[0]){
+    //Represents the Menu button being pushed
+    case 'M':
+      break;
+    //Represents the Start/Stop button being pushed
+    case 'S':
+      break;
+    //Represents the Up button being pushed
+    case 'U':
+      break;
+    //Represents the Down button being pushed
+    case 'D':
+      break;
+    //Represents the Left button being pushed
+    case 'L':
+      break;
+    //Represents the Right button being pushed
+    case 'R':
+      break;
+    default:
+      break;
+  }
 }
 
 
